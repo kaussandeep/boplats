5c48a2fa78100e53e6d373252707f1afab1fbc16
Sandeep Kaushik <CEC@qlik.com>
2018-06-28 16:45:30 +0200

    abc

--- test/1.js
+++ test/1.js
@@ -9,8 +9,20 @@ describe('This is first test', () => {
       const page = new SingleObjectPage();
       
       await page.get('https://nya.boplats.se/');
-      await browser.wait(EC.visibilityOf($('#pagehead')), 5000, 'failed');
-       
+      $('#login').click();
+      browser.wait(EC.visibilityOf($('#login-frame', 5000, 'failed')));
+      console.log('done');
+      $('. input').$('#username').sendKeys("abc");
+      var uname = $('[name="login_button"]');
+      //expect(uname.getText()).toBe('Log in');
+      //element.sendKeys("abcdef");
+     //browser.wait(EC.visibilityOf($('#username', 5, 'failed')));
+     //$('#username').focus();
+     
+
+      browser.sleep(6000);
+      //await browser.wait(EC.visibilityOf($('#pagehead')), 5000, 'failed');
+       //await browser.wait(EC.elementToBeClickable($('')));
      
       
     });
